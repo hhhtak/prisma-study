6788515ee1d61d7ccddd2b349823f27744296fe5
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-13 09:27:36 +0900

    Prismaクライアントを生成, Prisma Clientサービスの追加

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
+import { PrismaService } from './prisma.service'
 
 @Module({
   imports: [
@@ -11,6 +12,6 @@ import { join } from 'path'
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
