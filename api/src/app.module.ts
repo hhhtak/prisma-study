3172d955e3eb1e4339529a87eb404498a3cd38d5
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-14 08:35:14 +0900

    Users Module、Service、Resolverの作成

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
 import { PrismaService } from './prisma.service'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
