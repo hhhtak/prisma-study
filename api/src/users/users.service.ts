f6c60dbf598e2fed9c4ab1c743a689051cedecef
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-14 16:09:37 +0900

    Users Moduleを編集 Users Serviceを編集 Users Resolverを編集

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model'
+import { FindFirstUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/find-first-user.args'
+import { CreateOneUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/create-one-user.args'
+import { FindUniqueUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/find-unique-user.args'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findFirst = async (args: FindFirstUserArgs): Promise<User | null> => {
+    return this.prisma.user.findFirst(args)
+  }
+
+  findUnique = async (args: FindUniqueUserArgs): Promise<User | null> => {
+    return this.prisma.user.findUnique(args)
+  }
+
+  createUser = async (args: CreateOneUserArgs): Promise<User> => {
+    return this.prisma.user.create(args)
+  }
+}
