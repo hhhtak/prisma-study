43656fe4e92990c0f49fe9b67b3e4c4d6b13dcbe
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-26 08:46:43 +0900

    Auth Module、Service、Resolverの作成

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,6 +6,7 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { join } from 'path'
 import { PrismaService } from './prisma.service'
 import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module'
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
