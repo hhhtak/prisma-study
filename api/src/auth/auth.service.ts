22ca628cebb65437033419c7d7a606eaa699bd70
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-26 09:21:49 +0900

    AuthServiceを編集

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { UsersService } from 'src/users/users.service'
+import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model'
+import * as bcrypt from 'bcrypt'
+import { LoginResponse } from 'src/auth/dto/login-response'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService, private jwtService: JwtService) {}
+
+  validateUser = async (email: string, password: string): Promise<User | null> => {
+    const user = await this.userService.findUnique({ where: { email: email } })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return user
+    }
+    return null
+  }
+
+  login = async (user: User): Promise<LoginResponse> => {
+    const payload = { email: user.email, sub: user.id }
+    return {
+      access_token: this.jwtService.sign(payload),
+      user: user,
+    }
+  }
+}
