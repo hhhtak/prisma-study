bfa99494112b8a9c50c5702b45fd1f2d9a4d2709
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-30 23:16:52 +0900

    UserResolverの編集

--- api/src/users/users.resolver.ts
+++ api/src/users/users.resolver.ts
@@ -4,12 +4,15 @@ import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model'
 import { CreateOneUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/create-one-user.args'
 import { UsersService } from 'src/users/users.service'
 import { FindFirstUserArgs } from 'src/@generated/prisma-nestjs-graphql/user/find-first-user.args'
+import { UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
 @Resolver()
 export class UsersResolver {
   constructor(private readonly userService: UsersService) {}
 
   @Query(() => User)
+  @UseGuards(JwtAuthGuard)
   user(@Args() args: FindFirstUserArgs) {
     return this.userService.findFirst(args)
   }
