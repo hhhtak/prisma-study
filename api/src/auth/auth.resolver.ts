c0961604ce856df61df9a9f3ab848f61d0867d1b
takamatsu-hideomi <h_takamatsu02@kost.jp>
2022-09-30 23:13:53 +0900

    AuthResolverの編集

--- api/src/auth/auth.resolver.ts
+++ api/src/auth/auth.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common'
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
+import {} from '@nestjs/graphql'
+import { AuthService } from 'src/auth/auth.service'
+import { LoginResponse } from 'src/auth/dto/login-response'
+import { LoginUserInput } from 'src/auth/dto/login-user.input'
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard'
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => LoginResponse)
+  @UseGuards(GqlAuthGuard)
+  async login(@Args('loginUserInput') loginUserInput: LoginUserInput, @Context() context) {
+    return this.authService.login(context.user)
+  }
+}
